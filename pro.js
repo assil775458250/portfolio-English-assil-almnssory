635a235679c5bcdd1aff803707f9b60b0ea12c89
BMW <assilassilassil775458250@gmail.com>
2025-06-26 21:32:44 +0300

    Add files via upload

--- pro.js
+++ pro.js
@@ -60,3 +60,21 @@ window.onscroll =() =>
     navbar.classList.remove('active');
 
 };
+
+// scroll reveal
+ScrollReveal(
+    {
+        reset:true,
+        distance:'80px',
+        duration:2000,
+        delay:200
+
+    }
+);
+
+ScrollReveal().reveal('.home-content, .heading',{origin:'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-container,.contact form',{origin:'bottom'});
+ScrollReveal().reveal('.home-content h1',{origin:'left'});
+ScrollReveal().reveal('.home-content p',{origin:'right'});
+
+
